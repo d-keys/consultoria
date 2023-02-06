04847705a0443f66063f2b0269ea3de94cdc3484
d-keys <diamigo@tutanota.com>
2023-02-06 12:21:37 -0300

    Nome de função 'registrar'

--- assets/js/registrar.js
+++ assets/js/registrar.js
@@ -1,10 +1,10 @@
-function submit()
+function registrar()
 
 const nome = document.getElementById("nome")
 const email = document.getElementById("email")
 const tel = document.getElementById("tel")
 
-form.addEventlistener("submit", (e) =>{
+form.addEventlistener(submit, (e) =>{
 
 	var isValid = true
 		e.preventDefault();
@@ -12,24 +12,20 @@ form.addEventlistener("submit", (e) =>{
 	if (nome.value == "")
 		{window.alert("Por favor, preecha o campo Nome.");
 			document.nome.focus();
-			return false;
 	}
 
 	if (email.value == "")
 		{window.alert("Por favor, preecha o campo Email.");
 			document.nome.focus();
-			return false;
 	}
 
 	if (tel.value >= "")
 		{window.alert("Por favor, preecha o campo Telefone.");
 			document.nome.focus();
-			return false;
 	}
 
 	if (tel.value >= 12)
 		{window.alert("Por favor, preecha o campo Telefone corretamente.");
 			document.nome.focus();
-			return false;
 	}
 })
